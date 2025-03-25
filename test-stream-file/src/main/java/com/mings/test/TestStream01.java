a6c3425adf71cfd05e66273c7bdb0d0a7d34614c
qing520zhi <2507792033@qq.com>
2025-03-25 21:36:17 +0800

    2025/3/25  继续学习

---
+++ test-stream-file/src/main/java/com/mings/test/TestStream01.java
@@ -0,0 +1,71 @@
+package com.mings.test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.stream.Stream;
+
+/**
+ * @Author: MINGS
+ * @Date: 2025/03/25 20:47
+ * @Param:
+ * @Return:
+ * @Description:
+ **/
+public class TestStream01 {
+    public static void main(String[] args) {
+
+
+        //list();
+        //map();
+
+        //array();
+
+        num();
+    }
+
+    private static void num() {
+        Stream.of(1,2,3,5,4,7,6,2,3).filter(s -> s % 2 == 0).forEach(System.out::println);
+    }
+
+    /**
+     * @Author: MINGS
+     * @Date: 2025/03/25 20:57
+     * @Param:
+     * @Return:
+     * @Description:
+    **/
+    private static void array() {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        //数组流,静态方法获取流
+        Arrays.stream(arr).filter(s -> s % 2 == 0).forEach(System.out::println);
+    }
+
+    private static void map() {
+        //双列集合
+        HashMap<String, String> map = new HashMap<>();
+        map.put("name", "张三");
+        map.put("age", "18");
+        map.put("sex", "男");
+        map.put("address", "北京");
+
+        //双列集合不能直接获取stream流
+        //map.keySet().stream().filter(s -> s.startsWith("a")).forEach(System.out::println);
+        map.entrySet().stream().filter(s -> s.getKey().startsWith("a")).forEach(System.out::println);
+    }
+
+    private static void list() {
+
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("张三");
+        list.add("张五");
+        list.add("张大山");
+        list.add("李四");
+        list.add("王五");
+        list.add("赵六");
+
+        list.stream().filter(s -> s.startsWith("张")).filter(s -> s.length() == 2).forEach(System.out::println);
+    }
+}
